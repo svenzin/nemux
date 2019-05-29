a668d13b7842495b087d97472bf5ac7bb62fb00e
svenzin <stephane.venzin@gmail.com>
2019-05-29 22:21:29 +0200

    Add basic addressing od MMC1

--- nemux-test/Mapper-test-Mapper001.cpp
+++ nemux-test/Mapper-test-Mapper001.cpp
@@ -27,22 +27,50 @@ TEST_F(Mapper001Test, MMC1_IgnoreConsecutiveWrites) {
 }
 
 TEST_F(Mapper001Test, Mirroring_Single_Nametable0) {
-    FAIL();
+    mmc1.ScreenMode = Mapper_001::Mirroring::Screen0;
+    for (Word addr = 0x0000; addr < 0x0400; ++addr) {
+        EXPECT_EQ(addr, mmc1.NametableAddress(0x2000 + addr));
+        EXPECT_EQ(addr, mmc1.NametableAddress(0x2400 + addr));
+        EXPECT_EQ(addr, mmc1.NametableAddress(0x2800 + addr));
+        EXPECT_EQ(addr, mmc1.NametableAddress(0x2C00 + addr));
+    }
 }
 
 TEST_F(Mapper001Test, Mirroring_Single_Nametable1) {
-    FAIL();
+    mmc1.ScreenMode = Mapper_001::Mirroring::Screen1;
+    for (Word addr = 0x0000; addr < 0x0400; ++addr) {
+        EXPECT_EQ(0x0400 + addr, mmc1.NametableAddress(0x2000 + addr));
+        EXPECT_EQ(0x0400 + addr, mmc1.NametableAddress(0x2400 + addr));
+        EXPECT_EQ(0x0400 + addr, mmc1.NametableAddress(0x2800 + addr));
+        EXPECT_EQ(0x0400 + addr, mmc1.NametableAddress(0x2C00 + addr));
+    }
 }
 
 TEST_F(Mapper001Test, Mirroring_Horizontal) {
-    FAIL();
+    mmc1.ScreenMode = Mapper_001::Mirroring::Horizontal;
+    for (Word addr = 0x0000; addr < 0x0400; ++addr) {
+        EXPECT_EQ(addr, mmc1.NametableAddress(0x2000 + addr));
+        EXPECT_EQ(addr, mmc1.NametableAddress(0x2400 + addr));
+        EXPECT_EQ(0x0400 + addr, mmc1.NametableAddress(0x2800 + addr));
+        EXPECT_EQ(0x0400 + addr, mmc1.NametableAddress(0x2C00 + addr));
+    }
 }
 
-TEST_F(Mapper001Test, Mirroring_Single_Vertical) {
-    FAIL();
+TEST_F(Mapper001Test, Mirroring_Vertical) {
+    mmc1.ScreenMode = Mapper_001::Mirroring::Vertical;
+    for (Word addr = 0x0000; addr < 0x0400; ++addr) {
+        EXPECT_EQ(addr, mmc1.NametableAddress(0x2000 + addr));
+        EXPECT_EQ(0x0400 + addr, mmc1.NametableAddress(0x2400 + addr));
+        EXPECT_EQ(addr, mmc1.NametableAddress(0x2800 + addr));
+        EXPECT_EQ(0x0400 + addr, mmc1.NametableAddress(0x2C00 + addr));
+    }
 }
 
 TEST_F(Mapper001Test, MMC1_WriteToRegisters) {
+    FAIL();
+}
+
+TEST_F(Mapper001Test, MMC1_WriteIsAbove0x8000) {
     FAIL();
 }
 
@@ -59,43 +87,189 @@ TEST_F(Mapper001Test, ControlRegister) {
 }
 
 TEST_F(Mapper001Test, CHR0Register) {
+    // Include mirroring for banks too high
     FAIL();
+    //mmc1.ChrPages.resize(4);
+    //mmc1.WriteCHR0Register();
 }
 
 TEST_F(Mapper001Test, CHR1Register) {
+    // Include mirroring for banks too high
     FAIL();
 }
 
 TEST_F(Mapper001Test, PRGRegister) {
+    // Include mirroring for banks too high
     FAIL();
 }
 
 TEST_F(Mapper001Test, CHR_8KBanks) {
-    FAIL();
+    mmc1.ChrMode = Mapper_001::CHRBankingMode::OneBank;
+    
+    mmc1.ChrBank0 = 0;
+    mmc1.ChrBank1 = 0;
+    for (Word addr = 0; addr < 0x2000; addr++) {
+        const auto ba = mmc1.TranslatePpu(addr);
+        EXPECT_EQ(0, ba.Bank);
+        EXPECT_EQ(addr, ba.Address);
+    }
+
+    mmc1.ChrBank0 = 1;
+    mmc1.ChrBank1 = 0;
+    for (Word addr = 0; addr < 0x2000; addr++) {
+        const auto ba = mmc1.TranslatePpu(addr);
+        EXPECT_EQ(1, ba.Bank);
+        EXPECT_EQ(addr, ba.Address);
+    }
+
+    // Also check that CHR1 bank is ignored
+    mmc1.ChrBank0 = 1;
+    mmc1.ChrBank1 = 1;
+    for (Word addr = 0; addr < 0x2000; addr++) {
+        const auto ba = mmc1.TranslatePpu(addr);
+        EXPECT_EQ(1, ba.Bank);
+        EXPECT_EQ(addr, ba.Address);
+    }
 }
 
 TEST_F(Mapper001Test, CHR_4KBanks) {
-    FAIL();
+    mmc1.ChrMode = Mapper_001::CHRBankingMode::TwoBanks;
+
+    mmc1.ChrBank0 = 0;
+    mmc1.ChrBank1 = 1;
+    for (Word addr = 0; addr < 0x1000; addr++) {
+        const auto ba = mmc1.TranslatePpu(addr);
+        EXPECT_EQ(0, ba.Bank);
+        EXPECT_EQ(addr, ba.Address);
+    }
+    for (Word addr = 0; addr < 0x1000; addr++) {
+        const auto ba = mmc1.TranslatePpu(0x1000 + addr);
+        EXPECT_EQ(0, ba.Bank);
+        EXPECT_EQ(0x1000 + addr, ba.Address);
+    }
+
+    mmc1.ChrBank0 = 3;
+    mmc1.ChrBank1 = 2;
+    for (Word addr = 0; addr < 0x1000; addr++) {
+        const auto ba = mmc1.TranslatePpu(addr);
+        EXPECT_EQ(1, ba.Bank);
+        EXPECT_EQ(0x1000 + addr, ba.Address);
+    }
+    for (Word addr = 0; addr < 0x1000; addr++) {
+        const auto ba = mmc1.TranslatePpu(0x1000 + addr);
+        EXPECT_EQ(1, ba.Bank);
+        EXPECT_EQ(addr, ba.Address);
+    }
 }
 
 TEST_F(Mapper001Test, CHR_IsROM) {
-    FAIL();
+    const auto b = mmc1.GetPpuAt(0x0000);
+    mmc1.SetPpuAt(0x0000, b + 1);
+    EXPECT_EQ(b, mmc1.GetPpuAt(0x0000));
 }
 
 TEST_F(Mapper001Test, PRG_32KBanks) {
-    FAIL();
+    mmc1.PrgMode = Mapper_001::PRGBankingMode::SwitchAllBanks;
+
+    mmc1.PrgBank = 0;
+    for (Word addr = 0; addr < 0x4000; addr++) {
+        const auto ba = mmc1.TranslateCpu(0x8000 + addr);
+        EXPECT_EQ(0, ba.Bank);
+        EXPECT_EQ(addr, ba.Address);
+    }
+    for (Word addr = 0; addr < 0x4000; addr++) {
+        const auto ba = mmc1.TranslateCpu(0xC000 + addr);
+        EXPECT_EQ(1, ba.Bank);
+        EXPECT_EQ(addr, ba.Address);
+    }
+
+    mmc1.PrgBank = 2;
+    for (Word addr = 0; addr < 0x4000; addr++) {
+        const auto ba = mmc1.TranslateCpu(0x8000 + addr);
+        EXPECT_EQ(2, ba.Bank);
+        EXPECT_EQ(addr, ba.Address);
+    }
+    for (Word addr = 0; addr < 0x4000; addr++) {
+        const auto ba = mmc1.TranslateCpu(0xC000 + addr);
+        EXPECT_EQ(3, ba.Bank);
+        EXPECT_EQ(addr, ba.Address);
+    }
+
+    // Bit 0 of the bank is ignored
+    mmc1.PrgBank = 1;
+    for (Word addr = 0; addr < 0x4000; addr++) {
+        const auto ba = mmc1.TranslateCpu(0x8000 + addr);
+        EXPECT_EQ(0, ba.Bank);
+        EXPECT_EQ(addr, ba.Address);
+    }
+    for (Word addr = 0; addr < 0x4000; addr++) {
+        const auto ba = mmc1.TranslateCpu(0xC000 + addr);
+        EXPECT_EQ(1, ba.Bank);
+        EXPECT_EQ(addr, ba.Address);
+    }
 }
 
 TEST_F(Mapper001Test, PRG_16KBanks_FirstBank) {
-    FAIL();
+    const auto LastBank = mmc1.PrgPages.size() - 1;
+    mmc1.PrgMode = Mapper_001::PRGBankingMode::SwitchFirstBank;
+
+    mmc1.PrgBank = 0;
+    for (Word addr = 0; addr < 0x4000; addr++) {
+        const auto ba = mmc1.TranslateCpu(0x8000 + addr);
+        EXPECT_EQ(0, ba.Bank);
+        EXPECT_EQ(addr, ba.Address);
+    }
+    for (Word addr = 0; addr < 0x4000; addr++) {
+        const auto ba = mmc1.TranslateCpu(0xC000 + addr);
+        EXPECT_EQ(LastBank, ba.Bank);
+        EXPECT_EQ(addr, ba.Address);
+    }
+
+    mmc1.PrgBank = 1;
+    for (Word addr = 0; addr < 0x4000; addr++) {
+        const auto ba = mmc1.TranslateCpu(0x8000 + addr);
+        EXPECT_EQ(1, ba.Bank);
+        EXPECT_EQ(addr, ba.Address);
+    }
+    for (Word addr = 0; addr < 0x4000; addr++) {
+        const auto ba = mmc1.TranslateCpu(0xC000 + addr);
+        EXPECT_EQ(LastBank, ba.Bank);
+        EXPECT_EQ(addr, ba.Address);
+    }
 }
 
 TEST_F(Mapper001Test, PRG_16KBanks_LastBank) {
-    FAIL();
+    mmc1.PrgMode = Mapper_001::PRGBankingMode::SwitchLastBank;
+
+    mmc1.PrgBank = 0;
+    for (Word addr = 0; addr < 0x4000; addr++) {
+        const auto ba = mmc1.TranslateCpu(0x8000 + addr);
+        EXPECT_EQ(0, ba.Bank);
+        EXPECT_EQ(addr, ba.Address);
+    }
+    for (Word addr = 0; addr < 0x4000; addr++) {
+        const auto ba = mmc1.TranslateCpu(0xC000 + addr);
+        EXPECT_EQ(0, ba.Bank);
+        EXPECT_EQ(addr, ba.Address);
+    }
+
+    mmc1.PrgBank = 1;
+    for (Word addr = 0; addr < 0x4000; addr++) {
+        const auto ba = mmc1.TranslateCpu(0x8000 + addr);
+        EXPECT_EQ(0, ba.Bank);
+        EXPECT_EQ(addr, ba.Address);
+    }
+    for (Word addr = 0; addr < 0x4000; addr++) {
+        const auto ba = mmc1.TranslateCpu(0xC000 + addr);
+        EXPECT_EQ(1, ba.Bank);
+        EXPECT_EQ(addr, ba.Address);
+    }
 }
 
 TEST_F(Mapper001Test, PRG_IsROM) {
-    FAIL();
+    const auto b = mmc1.GetCpuAt(0x8000);
+    mmc1.SetCpuAt(0x8000, b + 1);
+    EXPECT_EQ(b, mmc1.GetCpuAt(0x8000));
 }
 
 TEST_F(Mapper001Test, LivePRGModeChange) {
--- nemux/Mapper_1.h
+++ nemux/Mapper_1.h
@@ -7,6 +7,11 @@
 
 class Mapper_001 : public NesMapper {
 public:
+    struct BankedAddress {
+        unsigned int Bank;
+        Word Address;
+    };
+
     enum class Mirroring {
         Screen0,
         Screen1,
@@ -33,6 +38,9 @@ public:
     Byte ChrBank1;
     bool RamEnabled;
 
+    std::vector<NesFile::PrgBank> PrgPages;
+    std::vector<NesFile::ChrBank> ChrPages;
+
     explicit Mapper_001(const NesFile & rom) {
         if (rom.Header.MapperNumber != 1) throw invalid_format("Invalid mapper (expected 001)");
         
@@ -47,20 +55,55 @@ public:
 
         PrgMode = PRGBankingMode::SwitchFirstBank;
         ChrMode = CHRBankingMode::OneBank;
+
+        PrgPages = rom.PrgRomPages;
+        ChrPages = rom.ChrRomPages;
     }
 
     virtual ~Mapper_001() {}
 
+    BankedAddress TranslateCpu(const Word address) const {
+        if (PrgMode == PRGBankingMode::SwitchFirstBank) {
+            if (address < 0xC000) return{ PrgBank, (address & 0x3FFF) };
+            return{ PrgPages.size() - 1, (address & 0x3FFF) };
+        }
+        if (PrgMode == PRGBankingMode::SwitchLastBank) {
+            if (address < 0xC000) return{ 0, (address & 0x3FFF) };
+            return{ PrgBank, (address & 0x3FFF) };
+        }
+        if (PrgMode == PRGBankingMode::SwitchAllBanks) {
+            const auto evenBank = PrgBank & 0xFE;
+            if (address < 0xC000) return{ evenBank, (address & 0x3FFF) };
+            return{ evenBank + 1, (address & 0x3FFF) };
+        }
+    }
+    
+    BankedAddress TranslatePpu(const Word address) const {
+        if (ChrMode == CHRBankingMode::OneBank) return{ ChrBank0, (address & 0x1FFF) };
+        if (ChrMode == CHRBankingMode::TwoBanks) {
+            const auto chr = ((address < 0x1000) ? ChrBank0 : ChrBank1);
+            return{ chr / 2, (chr % 2) * 0x1000 + (address & 0x0FFF) };
+        }
+    }
+
     Word NametableAddress(const Word address) const override {
+        if (ScreenMode == Mirroring::Screen0) return (address & 0x03FF);
+        if (ScreenMode == Mirroring::Screen1) return (0x0400 | (address & 0x03FF));
+        if (ScreenMode == Mirroring::Vertical) return (address & 0x7FF);
+        if (ScreenMode == Mirroring::Horizontal) return (((address & 0x0800) >> 1) | (address & 0x03FF));
     }
 
     Byte GetCpuAt(const Word address) const override {
+        const auto addr = TranslateCpu(address);
+        return PrgPages[addr.Bank][addr.Address];
     }
 
     void SetCpuAt(const Word address, const Byte value) override {
     }
 
     Byte GetPpuAt(const Word address) const override {
+        const auto addr = TranslatePpu(address);
+        return ChrPages[addr.Bank][addr.Address];
     }
 
     void SetPpuAt(const Word address, const Byte value) override {
