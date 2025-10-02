d87c77a48fda13f35829121af5d4cd3a89d3a8a3
svenzin <stephane.venzin@gmail.com>
2025-10-03 01:27:18 +0200

    Cleanup in Bit utilities

--- nemux/BitUtil.h
+++ nemux/BitUtil.h
@@ -1,24 +1,23 @@
-#ifndef BIT_UTIL_H_
-#define BIT_UTIL_H_
-
-#include <cstddef>
+#pragma once
 
 #include "Types.h"
 
-template <std::size_t bit> constexpr Byte Mask(const bool & isset) {
+template <std::size_t bit>
+constexpr Byte Mask(bool isset) {
     return isset ? 0x01 << bit : 0x00;
 }
 
-template <std::size_t bit> constexpr Flag Bit(const Byte & value) {
+template <std::size_t bit>
+constexpr Flag Bit(Byte value) {
     return (value >> bit) & 0x01;
 }
 
-template <std::size_t bit> constexpr bool IsBitSet(const Byte & value) {
+template <std::size_t bit>
+constexpr bool IsBitSet(Byte value) {
     return Bit<bit>(value) == 1;
 }
 
-template <std::size_t bit> constexpr bool IsBitClear(const Byte & value) {
+template <std::size_t bit>
+constexpr bool IsBitClear(Byte value) {
     return !IsBitSet<bit>(value);
 }
-
-#endif
